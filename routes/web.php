25928a8debd27e238137b0c712bcac7fc43c6b3e
RicardsAlmanis <you@example.com>
2025-10-29 14:03:55 +0200

    updated web.php

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,26 @@
 <?php
 
-use App\Http\Controllers\ItemController;
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ItemController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
+
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    Route::resource('items', ItemController::class);
+
+    Route::get('/search', [ItemController::class, 'search'])->name('items.search');
+    Route::get('/filter/{status}', [ItemController::class, 'filter'])->name('items.filter');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::resource('posts', ItemController::class)->middleware('auth');
 require __DIR__.'/auth.php';
-
-Route::resource('items', ItemController::class)->middleware('auth');
