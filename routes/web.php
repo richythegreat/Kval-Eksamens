4c44a5b237d364199025302ecd9da5895fdbe00b
RicardsAlmanis <you@example.com>
2025-10-29 13:57:18 +0200

    updated item controller, added views for items, added route for item

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ItemController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -19,3 +20,5 @@ Route::middleware('auth')->group(function () {
 
 Route::resource('posts', ItemController::class)->middleware('auth');
 require __DIR__.'/auth.php';
+
+Route::resource('items', ItemController::class)->middleware('auth');
