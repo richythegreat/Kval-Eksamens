47f1703506e02474d74f6833d326fd16f1fe7cb0
RicardsAlmanis <you@example.com>
2025-10-29 14:06:33 +0200

    updated web.php

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ItemController;
+use App\Http\Controllers\ProfileController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware('auth')->group(function () {
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+Route::middleware('auth')->group(function () {
 
     Route::resource('items', ItemController::class);
 
